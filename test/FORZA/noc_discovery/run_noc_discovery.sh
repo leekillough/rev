d9cb25f8dc4ad8bb559d5f63d256deb5d3fbedb9
Ryan Kabrick <rkabrick@udel.edu>
2023-12-19 08:38:21 -0600

    Adding proper handling of noc_discovery test

--- test/FORZA/noc_discovery/run_noc_discovery.sh
+++ test/FORZA/noc_discovery/run_noc_discovery.sh
@@ -4,9 +4,9 @@
 make clean && make
 
 # Check that the exec was built...
-if [ -f ex2.exe ]; then
-  sst --add-lib-path=../../build/src/ ./rev-test-noc-discovery.py
+if [ -f noc_discovery.exe ]; then
+	sst --add-lib-path=../../build/src/ ./rev-test-noc-discovery.py
 else
-  echo "Test EX2: ex2.exe not Found - likely build failed"
-  exit 1
+	echo "Test NOC_DISCOVERY: noc_discovery.exe not Found - likely build failed"
+	exit 1
 fi
