061d4642d48533e701dbe9a2a7ed2eb8a0d11754
jleidel <john.leidel@gmail.com>
2023-12-28 10:13:17 -0600

    splitting the forza-specific componentry into separate library build+install

--- test/FORZA/forza_amoadd_c/rev-amoadd_c.py
+++ test/FORZA/forza_amoadd_c/rev-amoadd_c.py
@@ -128,10 +128,10 @@ rtr_params = {
   "num_ports" : "2",
   "id" : 0
 }
-zap_nic = zap_cpu1.setSubComponent("zone_nic", "Forza.zopNIC")
+zap_nic = zap_cpu1.setSubComponent("zone_nic", "forza.zopNIC")
 zap_iface = zap_nic.setSubComponent("iface", "merlin.linkcontrol")
 
-rza_nic = rza.setSubComponent("zone_nic", "Forza.zopNIC")
+rza_nic = rza.setSubComponent("zone_nic", "forza.zopNIC")
 rza_iface = rza_nic.setSubComponent("iface", "merlin.linkcontrol")
 
 router = sst.Component("router", "merlin.hr_router")
