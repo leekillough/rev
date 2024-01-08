aeb593da3417476e96008afd7906cb4710be60c4
Ryan Kabrick <rkabrick@udel.edu>
2024-01-08 13:08:38 -0500

    Commenting out (potentially) redundant copying of Register Files pre-migration

--- include/insns/RV64A.h
+++ include/insns/RV64A.h
@@ -31,12 +31,12 @@ class RV64A : public RevExt {
       for( unsigned i=1; i<32; i++ ){
         P.push_back(R->GetX<uint64_t>(i));
       }
-      for( unsigned i=0; i<32; i++ ){
-        uint64_t t = 0x00ull;
-        double s= R->DPF[i];
-        memcpy(&t, &s, sizeof(t));
-        P.push_back(t);
-      }
+      //for( unsigned i=0; i<32; i++ ){
+      //  uint64_t t = 0x00ull;
+      //  double s= R->DPF[i];
+      //  memcpy(&t, &s, sizeof(t));
+      //  P.push_back(t);
+      //}
       R->SetSCAUSE(EXCEPTION_CAUSE::THREAD_MIGRATED);
       return M->ZOP_ThreadMigrate(F->GetHartToExecID(), P, Zone, Precinct);
     }
@@ -58,12 +58,12 @@ class RV64A : public RevExt {
       for( unsigned i=1; i<32; i++ ){
         P.push_back(R->GetX<uint64_t>(i));
       }
-      for( unsigned i=0; i<32; i++ ){
-        uint64_t t = 0x00ull;
-        double s= R->DPF[i];
-        memcpy(&t, &s, sizeof(t));
-        P.push_back(t);
-      }
+//      for( unsigned i=0; i<32; i++ ){
+//        uint64_t t = 0x00ull;
+//        double s= R->DPF[i];
+//        memcpy(&t, &s, sizeof(t));
+//        P.push_back(t);
+//      }
       R->SetSCAUSE(EXCEPTION_CAUSE::THREAD_MIGRATED);
       return M->ZOP_ThreadMigrate(F->GetHartToExecID(), P, Zone, Precinct);
     }
@@ -90,13 +90,13 @@ class RV64A : public RevExt {
       for( unsigned i=1; i<32; i++ ){
         P.push_back(R->GetX<uint64_t>(i));
       }
-      for( unsigned i=0; i<32; i++ ){
-        uint64_t t = 0x00ull;
-        double s= R->DPF[i];
-        memcpy(&t, &s, sizeof(t));
-        P.push_back(t);
-      }
-
+//      for( unsigned i=0; i<32; i++ ){
+//        uint64_t t = 0x00ull;
+//        double s= R->DPF[i];
+//        memcpy(&t, &s, sizeof(t));
+//        P.push_back(t);
+//      }
+//
       R->SetSCAUSE(EXCEPTION_CAUSE::THREAD_MIGRATED);
       return M->ZOP_ThreadMigrate(F->GetHartToExecID(), P, Zone, Precinct);
     }
