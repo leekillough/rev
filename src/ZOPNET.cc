c154ee92ce512769050fce0dcfc4dd52b5b37d73
jleidel <john.leidel@gmail.com>
2024-01-10 09:02:16 -0600

    adding handlers for non-precinct local requests from a ZEN

--- src/ZOPNET.cc
+++ src/ZOPNET.cc
@@ -273,6 +273,15 @@ void zopNIC::send(zopEvent *ev, zopCompID dest, zopPrecID zone, unsigned prec){
     preInitQ.push_back(std::make_pair(ev, TmpDest));
     return ;
   }
+
+  // check to see whether the designated destination is within the same precinct
+  if( (getEndpointType() == Forza::zopCompID::Z_ZEN) &&
+      (prec != getPrecinctID()) ){
+    // this is a ZEN device and the target location is outside the local precint
+    // route the request to the ZIP device
+    ev->setDestPCID((uint8_t)(Forza::zopPrecID::Z_ZIP));
+  }
+
   SST::Interfaces::SimpleNetwork::Request *req =
     new SST::Interfaces::SimpleNetwork::Request();
   output.verbose(CALL_INFO, 9, 0,
