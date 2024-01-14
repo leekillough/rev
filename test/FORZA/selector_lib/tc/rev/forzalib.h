25206e6e0c5cc008d22661cd08c11185aa834146
Ajay Mandadi <amandadi3@gatech.edu>
2024-01-14 00:44:34 -0500

    tc for rev

--- test/FORZA/selector_lib/tc/rev/forzalib.h
+++ test/FORZA/selector_lib/tc/rev/forzalib.h
@@ -1,39 +1,30 @@
 
-#include <inttypes.h>
-#include <cstring>
-#include <cstdio>
+#include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include "forza_utils.h"
 
-
-// #define THREADS 1
-#define THREADS 4
-#define BUF_SIZE 50000
-
-typedef struct barrier_struct {
-    volatile uint64_t t[THREADS];
-} barrier_t;
-
-barrier_t DEFAULT_BARRIER_INSTANCE = {.t = {0ul}};
-
-// void barrier(volatile barrier_t *p, int mythread) {
-void forza_barrier(uint64_t mythread) {
-    volatile barrier_t *p = &DEFAULT_BARRIER_INSTANCE;
-
-    p->t[mythread]++;
-    uint64_t a = p->t[mythread];
-    while (1) {
-        int ok = 1;
-        for (int i = 0; i < THREADS; i++) {                                                                                                                                                                                                                                                                                                                      
-            if (p->t[i] < a) {
-                ok = 0;
-            }
-        }
-        if (ok) {
-            break;
-        }
-    }
+int forza_thread_create(forza_thread_t *tid, void *fn, void *arg)
+{
+    int ret;
+    ret = rev_pthread_create(tid, NULL, fn, arg);
+    return ret;
 }
+
+int forza_thread_join(forza_thread_t tid)
+{
+    int ret;
+    ret = rev_pthread_join(tid);
+    return ret;
+}
+
+uint64_t forza_file_read(char *filename, char *buf, size_t count)
+{
+    int fd = rev_openat(AT_FDCWD, filename, 0, O_RDONLY);
+    uint64_t bytesRead = rev_read(fd, buf, count);
+    rev_close(fd);
+    return bytesRead;
+}
+
 
 typedef struct sparsemat_t_ {
   int64_t local;                //!< 0/1 flag specifies whether this is a local or distributed matrix
@@ -45,21 +36,21 @@ typedef struct sparsemat_t_ {
   int64_t nnz;                  //!< total number of nonzeros in the matrix
   int64_t lnnz;                 //!< the number of nonzeros on this PE
   //int64_t * offset;      //!< the row offsets into the array of nonzeros
-  int64_t loffset[10000];            //!< the row offsets for the row with affinity to this PE
+  int64_t loffset[1000];            //!< the row offsets for the row with affinity to this PE
   //int64_t * nonzero;     //!< the global array of column indices of nonzeros
-  int64_t lnonzero[10000];           //!< local array of column indices (for rows on this PE).
+  int64_t lnonzero[1000];           //!< local array of column indices (for rows on this PE).
   //double * value;        //!< the global array of values of nonzeros. Optional.
   //double * lvalue;              //!< local array of values (values for rows on this PE)
 }sparsemat_t;
 
 typedef struct TrianglePkt {
-      int64_t w;
-     volatile int64_t vj;
-    volatile int valid;
+    int64_t w;
+    int64_t vj;
+    volatile int64_t valid;
 } TrianglePkt;
 
 typedef struct mailbox_t {
-    TrianglePkt pkt[1000000];
+    TrianglePkt pkt[1000];
     uint64_t send_count;
     uint64_t recv_count;
     volatile int mbdone;
@@ -80,31 +71,23 @@ void generate_graph(sparsemat_t *mat, int mynode)
     switch(mynode)
     {
       case 0:
-          strcpy(path,"data/n4s8/node0.dat");
+          strcpy(path,"node0.dat");
           break;
       case 1:
-          strcpy(path,"data/n4s8/node1.dat");
+          strcpy(path,"node1.dat");
           break;
       case 2:
-          strcpy(path,"data/n4s8/node2.dat");
+          strcpy(path,"node2.dat");
           break;
       case 3:
-          strcpy(path,"data/n4s8/node3.dat");
+          strcpy(path,"node3.dat");
           break;
       default:
           break;        
     }
 
-    // int fd = rev_openat(AT_FDCWD, path, 0, O_RDONLY);
-    // uint64_t bytesRead = rev_read(fd, buffer1, 600);
-    FILE *fptr;
-    fptr = fopen(path, "r");
-    fread(buffer1, 50000, 1, fptr);
-    size_t bytesRead = strlen(buffer1);
-    // printf("AJAY[%d]: bytes read - %ld %s\n", mynode, strlen(buffer1), buffer1);
-
-    // uint64_t bytesRead = forza_file_read(path, buffer1, 50000);
-    // buffer1[bytesRead] = '\0';
+    uint64_t bytesRead = forza_file_read(path, buffer1, 50000);
+    buffer1[bytesRead] = '\0';
 
     uint64_t j = 0;
     uint64_t k = 0;
@@ -141,7 +124,6 @@ void generate_graph(sparsemat_t *mat, int mynode)
     {
         mat[mynode].lnonzero[lzeros] = atoi(buffer2[zero_offset + lzeros]);
     }
-    fclose(fptr);
     return;
 }
 
@@ -158,21 +140,8 @@ void forza_done(mailbox mb[][4], int src)
 {
     for(int i = 0; i < THREADS; i++)
     {
-        // printf("AJAY: marking %d %d as done\n", i ,src);
         mb[i][src].mbdone = 1;
     }
-}
-
-int isAllDone(int mythread)
-{
-    for(int i = 0; i < THREADS; i++)
-    {
-        if(mb_request[mythread][i].mbdone == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
 }
 
 void forza_recv_process(TrianglePkt pkg, int AID)
@@ -192,15 +161,14 @@ void forza_recv_process(TrianglePkt pkg, int AID)
     cnt[AID] += tempCount;
 }
 
-void *forza_poll_thread(void *mytid)
+void *forza_poll_thread(int *mytid)
 {
-    int mythread = *(int *) mytid;
+    int mythread = *mytid;
     volatile int done_flag = 1;
     
     while(1)
     {
         done_flag = 1;
-        // done_flag = isAllDone(mythread);
         for(int i = 0; i < THREADS; i++)
         {
             if(mb_request[mythread][i].mbdone == 0)
@@ -213,6 +181,8 @@ void *forza_poll_thread(void *mytid)
         for(int i = 0; i < THREADS; i++)
         {
             uint64_t recvcnt = mb_request[mythread][i].recv_count;
+            print_args[0] = (void *) &recvcnt;
+
             while(mb_request[mythread][i].pkt[recvcnt].valid)
             {
                 forza_recv_process(mb_request[mythread][i].pkt[recvcnt], mythread);
