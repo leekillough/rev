5932dd559fdbf1d13b99e99c6873321b1f7d4b65
Seonjin Na <sna42@gatech.edu>
2024-01-14 14:31:20 -0500

    update file names

--- test/FORZA/forza_security/run_shared_invalid.sh
+++ test/FORZA/forza_security/run_shared_invalid.sh
@@ -4,28 +4,9 @@
 make clean && make
 
 # Check that the exec was built...
-if [ -f amoadd_c.exe ]; then
-  sst --add-lib-path=../../build/src/ ./shared-invalid.py | tee shared_invalid.log
-  # sst --add-lib-path=../../build/src/ ./shared-valid.py
-
-
+if [ -f forza_security.exe ]; then
+  sst --add-lib-path=../../build/src/ ./shared-invalid.py 
 else
-  echo "Test FORZA amoadd_c: amoadd_c.exe not Found - likely build failed"
+  echo "Test FORZA Security: forza_security.exe not Found - likely build failed"
   exit 1
 fi
-
-
-# count the number of Invalid PAddr
-count=$(grep -c "Invalid PAddr" shared_invalid.log)
-
-if [ "$count" -gt 0 ]; then
-    echo "Test Passed"
-    result=0
-else
-    echo "Test Failed"
-    result=1
-fi
-
-rm -f shared_invalid.log
-
-exit $result
