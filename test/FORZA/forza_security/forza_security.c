d41664f1b1b591ab6033cf92da0e27cc8eecd837
Seonjin Na <sna42@gatech.edu>
2024-01-15 15:35:59 -0500

    indent

--- test/FORZA/forza_security/forza_security.c
+++ test/FORZA/forza_security/forza_security.c
@@ -6,13 +6,15 @@ uint32_t atom32 = 0;
 uint64_t A[8192];
 uint64_t B[8192];
 uint64_t R[8192];
-int main() {
-  uint64_t i = 0;
-  uint64_t j = 0;
-  int r = 0;
-  for( i=0; i<8192; i++ ){
-      R[i] = A[i] + B[i] * i;
-  }
+int main()
+{
+    uint64_t i = 0;
+    uint64_t j = 0;
+    int r = 0;
+    for (i = 0; i < 8192; i++)
+    {
+        R[i] = A[i] + B[i] * i;
+    }
 
     return 0;
 }
