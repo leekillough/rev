e576058b8c004056d9c2fe862a3e67d277430dc3
Tim Dysart <tdysart@lucata.com>
2024-01-24 13:30:16 -0500

    Clean up some naming; add a couple of comments.

--- src/zen/zen.h
+++ src/zen/zen.h
@@ -135,7 +135,7 @@ namespace SST::Forza{
     // private class members
 
     void sendMsgToRZADMA(uint64_t acs, uint64_t addr,
-                         std::vector<uint64_t> src_payload, uint8_t msg_id,
+                         std::vector<uint64_t> src_payload, uint8_t cur_msg_id,
                          uint64_t hart_id, uint64_t queue_loc);
     void sendMsgToRZANonDMA(uint64_t acs, uint64_t addr, uint64_t src_payload,
                             uint8_t cur_msg_id, uint64_t hart_id,
