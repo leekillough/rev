7ed32b213a38205591a62ff7f6844f116f5960f2
Tim Dysart <tdysart@tactcomplabs.com>
2024-05-30 15:07:23 -0400

    quick clean up pass

--- src/zen/zen.cc
+++ src/zen/zen.cc
@@ -11,29 +11,28 @@ namespace SST::Forza{
 
 uint64_t ZenMailboxMetadata::getRzaWriteAddr(uint8_t size)
 {
-  uint64_t wr_ptr = mem_wr_ptr;
+  uint64_t wr_ptr = mem_wr_ptr; // address to write
   uint64_t next_ptr = wr_ptr + (size * sizeof(uint64_t));
   if (next_ptr > mem_tail) //shouldn't happen
-    //output.fatal(CALL_INFO, -1, "Packet will be stored in non-contiguous memory");
     return (uint64_t)-1;
   else if (next_ptr == mem_tail)
     next_ptr = mem_head;
   
-  mem_wr_ptr = next_ptr;
+  mem_wr_ptr = next_ptr; // update write ptr
   return wr_ptr;
 }
 
+// This is called AFTER we've written memory
 uint64_t ZenMailboxMetadata::getSpTailAddr(uint8_t size)
 {
-  uint64_t wr_ptr = mem_cur_tail;
+  uint64_t wr_ptr = mem_cur_tail; //current tail; we wrote to this address
   uint64_t next_ptr = wr_ptr + (size * sizeof(uint64_t));
   if (next_ptr > mem_tail) //shouldn't happen
-    //output.fatal(CALL_INFO, -1, "Packet will be stored in non-contiguous memory");
     return (uint64_t)-1;
   else if (next_ptr == mem_tail)
     next_ptr = mem_head;
   
-  mem_cur_tail = next_ptr;
+  mem_cur_tail = next_ptr; // update to past where we've written
   return mem_cur_tail;
 }
 
@@ -237,11 +236,11 @@ void ZEN::helper_handleFromPrecMsgZop(SST::Forza::zopEvent *ev)
 void ZEN::handleIncomingZOP(SST::Event *event) {
   SST::Forza::zopEvent* ev = static_cast<SST::Forza::zopEvent*>(event);
 
-  output.verbose(CALL_INFO, 7, 0, "[ZEN]: %s received msg type %s @ [hart:zcid:pcid:id:type]=[%d:%d:%d:%hu:%s]\n",
+  output.verbose(CALL_INFO, 7, 0, "[ZEN]: %s received msg_id=%u from %s to %s\n",
                  getName().c_str(),
-                 m_zop_iface->msgTToStr(ev->getType()).c_str(),
-                 ev->getSrcHart(), ev->getSrcZCID(), ev->getSrcPCID(), ev->getID(),
-                 m_zop_iface->endPToStr(m_zop_iface->getEndpointType()).c_str());
+                 ev->getID(),
+                 ev->getSrcString().c_str(),
+                 ev->getDestString().c_str());
 
   // Sanity check for incoming packets
   if (!isSrcLocal(ev))
@@ -265,12 +264,12 @@ void ZEN::handleIncomingZOP(SST::Event *event) {
     case SST::Forza::zopMsgT::Z_RZOP:
       // Memory zop type - should be strictly outgoing to precinct NoC
       if (isDestLocal(ev))
-        output.fatal(CALL_INFO, -1, "ZEN %s: received a memory zop with local dest\n",
+        output.fatal(CALL_INFO, -2, "ZEN %s: received a memory zop with local dest\n",
                      getName().c_str());
       // TODO: Handle credits?
       // Put packet in outgoing queue
       // to_precinct_noc_q.push_back(ev);
-      output.fatal(CALL_INFO, -1, "ZEN %s: received an outgoing memory zop packet (unhandled)\n",
+      output.fatal(CALL_INFO, -3, "ZEN %s: received an outgoing memory zop packet (unhandled)\n",
                    getName().c_str());
       break;
 
@@ -278,18 +277,18 @@ void ZEN::handleIncomingZOP(SST::Event *event) {
       // Thread migration type; for now, thrown an error
       //    Should be to a different zone (any precinct);
       //    and just pass through here
-      output.fatal(CALL_INFO, -1, "ZEN %s: received a thread migration packet (unhandled)\n",
+      output.fatal(CALL_INFO, -4, "ZEN %s: received a thread migration packet (unhandled)\n",
         getName().c_str());
       break;
 
     case SST::Forza::zopMsgT::Z_EXCP:
       // Exception type; for now, throw an error
-      output.fatal(CALL_INFO, -1, "ZEN %s: received an exception packet (unhandled)\n",
+      output.fatal(CALL_INFO, -5, "ZEN %s: received an exception packet (unhandled)\n",
         getName().c_str());
       break;
 
     default: // Z_TMGT, Z_SYSC, Z_FENCE: don't expect to see here (as of now)
-      output.fatal(CALL_INFO, -1, "ZEN %s: received an unexpected packet type\n",
+      output.fatal(CALL_INFO, -6, "ZEN %s: received an unexpected packet type\n",
         getName().c_str());
       break;
   }
@@ -965,9 +964,6 @@ void ZEN::prepSendRZAStore() {
       output.fatal(CALL_INFO, -1, "Invalid number of msg_ids returned\n");
     }
 
-    // Testing hack
-    msg_ids[0] = 52;
-
     // Get destination address for memory zop(s)
     auto mbox_info = getDestMboxEntry(ev);
     uint64_t wr_addr = mbox_info->getRzaWriteAddr(ev->getLength());
@@ -1128,7 +1124,6 @@ void ZEN::processSetupMsgs(){
     out_payload.push_back(0);
     out_payload.push_back(scratch_tail);
     out_payload.push_back(mem_start_addr);
-    //sendMsgToScratchpad(hart_id, zcid, scratch_tail, 1, mem_start_addr); 
     sendMsgToScratchpad(ev, out_payload, msg_id, true);
 
     output.verbose(CALL_INFO, 9, 0, "Zen Setup, HartID %" PRIu64 ", start addr 0x%" PRIx64 ", end addr 0x%" PRIx64 "\n",
