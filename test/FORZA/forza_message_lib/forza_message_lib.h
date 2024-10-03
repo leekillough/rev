26666301d0a4662e2c3634b3c5ff0e5e7585026b
Shannon Kuntz <skuntz@tactcomplabs.com>
2024-10-03 16:11:26 -0500

    Add functions to return current number of outstanding messages for a mailbox or loop until no remaining messages in the mailbox

--- test/FORZA/forza_message_lib/forza_message_lib.h
+++ test/FORZA/forza_message_lib/forza_message_lib.h
@@ -3,7 +3,6 @@
  * @brief Interface to HW for low-level actor message send and receive support
  */
 
-
 #include "../../../common/syscalls/forza.h"
 #include "../../../common/syscalls/syscalls.h"
 #include <stdbool.h>
@@ -17,19 +16,16 @@
     }                             \
   while( 0 )
 
-#define DEBUG 0
-#define NMBOX 8UL  // HW restricts the number of mailboxes to 8
+#define DEBUG         0
+#define NMBOX         8UL  // HW restricts the number of mailboxes to 8
 #define ZQM_COMPONENT 0UL
 
 /**
  * @brief Message op types
  */
-enum MSG_OP {
-	DIRECT_OP,
-	INDIRECT_OP,
-	DONE_OP
-};
-#define MSG_OP_MASK 0x0000FF0000000000UL   // MSG_OP [47..40]
+enum MSG_OP { DIRECT_OP, INDIRECT_OP, DONE_OP };
+
+#define MSG_OP_MASK 0x0000FF0000000000UL  // MSG_OP [47..40]
 
 /**
  * @brief Return Status Codes
@@ -37,7 +33,7 @@ enum MSG_OP {
 enum RET_STATUS {
   SUCCESS,
   DONE_MSG,
-	NO_MSG,
+  NO_MSG,
   INVALID_NUM_MBOX,
   INVALID_MBOX,
   INVALID_LPE,
@@ -45,7 +41,7 @@ enum RET_STATUS {
   INVALID_ZONE,
   ZEN_NOT_READY,
   INVALID_PTR,
-	INVALID_PKT_SZ,
+  INVALID_PKT_SZ,
   MALLOC_FAIL
 };
 
@@ -58,35 +54,73 @@ enum RET_STATUS {
  */
 int forza_get_num_logical_pes() {
 
-	return(forza_get_harts_per_zap() * forza_get_zaps_per_zone());
+  return ( forza_get_harts_per_zap() * forza_get_zaps_per_zone() );
 }
 
 /**
  * @brief Registers this thread's logical PE with the ZQM so that it can correctly
- * 	deliver messages 
+ * 	deliver messages
  *
  */
-int forza_zqm_pe_setup( uint64_t logical_pe, uint64_t n_mailboxes) {
+int forza_zqm_pe_setup( uint64_t logical_pe, uint64_t n_mailboxes ) {
 
   // Check inputs
-  if (logical_pe > forza_get_num_logical_pes() - 1) {
-	  #if DEBUG
-		char msg[55] = "\ndebug: forza_zqm_pe_setup: INVALID_LPE\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		#endif
+  if( logical_pe > forza_get_num_logical_pes() - 1 ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_zqm_pe_setup: INVALID_LPE\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_LPE;
-	}
-  if (n_mailboxes > NMBOX) {
-		#if DEBUG
-		char msg[55] = "\ndebug: forza_zqm_pe_setup: INVALID_NUM_MBOX\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		#endif
+  }
+  if( n_mailboxes > NMBOX ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_zqm_pe_setup: INVALID_NUM_MBOX\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_NUM_MBOX;
-	}
-  forza_zqm_setup(logical_pe, n_mailboxes);
+  }
+  forza_zqm_setup( logical_pe, n_mailboxes );
   return SUCCESS;
 }
 
+/**
+ *  @brief Returns the number of outstanding messages for this mailbox
+ *  I.e. the number of messages that have not yet been acknowledged
+ *
+ */
+uint64_t forza_get_outstanding_msg_count( uint64_t mb_id ) {
+  uint64_t shift    = 8 * mb_id;
+  uint64_t mb_mask  = 0xFFUL << shift;
+  uint64_t mb_count = forza_zen_get_cntrs() & mb_mask;
+
+  return mb_count >> shift;
+}
+
+/** @brief Checks the ZEN mailbox counters for the specified mailbox to see if
+ *  all messages have been delivered
+ */
+int forza_wait_mailbox_done( uint64_t mb_id ) {
+  uint64_t shift    = 8 * mb_id;
+  uint64_t mb_mask  = 0xFFUL << shift;
+  uint64_t mb_count = forza_zen_get_cntrs() & mb_mask;
+
+#if 1
+  char msg[55] = "\ndebug: forza_wait_mailbox_done: MB=X Count=Y\n";
+  msg[36]      = '0' + mb_id;
+  msg[44]      = '0' + mb_count;
+  rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+  while( mb_count != 0 ) {
+    mb_count = forza_zen_get_cntrs() & mb_mask;
+#if 1
+    char msg[55] = "\ndebug: forza_wait_mailbox_done: MB=X Count=Y\n";
+    msg[36]      = '0' + mb_id;
+    msg[44]      = '0' + mb_count;
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+  }
+  return SUCCESS;
+}
 
 /**
  * @brief Send a packet to the specified mailbox at the destination precinct:zone:logical_pe
@@ -94,141 +128,151 @@ int forza_zqm_pe_setup( uint64_t logical_pe, uint64_t n_mailboxes) {
  * The packet must point to 56b of payload and will be sent as 7 64b words
  *
  */
-int forza_send(uint64_t mb_id, void * pkt, uint64_t precinct, uint64_t zone, uint64_t logical_pe) {
-
-	// Check for valid inputs
-	if (mb_id >= NMBOX) {
-		#if DEBUG
-    char msg[55] = "\ndebug: forza_send: INVALID_MBOX\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		#endif
-		return INVALID_MBOX;
-	}
-	if (pkt == NULL) {
-	  #if DEBUG
-    char msg[60] = "\ndebug: forza_send: INVALID_PTR, pkt == NULL\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		#endif
-		return INVALID_PTR;
-	}
-	if (precinct >= forza_get_num_precincts()) {
-		#if DEBUG
-    char msg[55] = "\ndebug: forza_send: INVALID_PRECINCT\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
-		return INVALID_PRECINCT;
-	}
-	if (zone >= forza_get_zones_per_precinct()) {
-	  #if DEBUG
-    char msg[55] = "\ndebug: forza_send: INVALID_ZONE\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
-		return INVALID_ZONE;
-	}
-	if (logical_pe >= forza_get_num_logical_pes()) {
-	 	#if DEBUG
-    char msg[55] = "\ndebug: forza_send: INVALID_LPE\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
- 		return INVALID_LPE;
- 	}
-
-	// Check zen status
-	uint64_t zenstat = forza_read_zen_status();
-  if (zenstat == 0) {
-		char msg[55] = "\ndebug: forza_send: zenstat error\n";
-  	rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		assert(0); 
-	}
-
- 	// Send 7 data words
-	uint64_t * data = pkt;
-	for (int i=0; i<7; i++)
- 		forza_send_word( data[i], false );
-  
-	// Create the control word
-  uint64_t ctrl_word = mb_id << 33;
-	ctrl_word |= precinct << 20;
-	ctrl_word != zone << 16;
-	//ctrl_word |= ZQM_COMPONENT << 12; 
-  ctrl_word |= logical_pe;
-  forza_send_word( ctrl_word, true );
-
-	uint64_t cntrs = forza_zen_get_cntrs();
- 
- 	// Debug 0x2222 send: control word and counteri
-	#if DEBUG
-	{char msg[50] = "\ndebug: forza_send: 0x2222, ctrl_word, counters\n";
-  rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-	forza_debug_print( 0x2222, ctrl_word, cntrs);
-	#endif
-
-	return SUCCESS;
-}
-
-
-/** @brief Sends a done message to the mailbox at the destination precinct:zone:logocal_pe
- *   to indiocate that no more messages will be sent from this PE to the destination mailbox
- *
- */
-int forza_send_done(uint64_t mb_id, uint64_t precinct, uint64_t zone, uint64_t logical_pe) {
+int forza_send( uint64_t mb_id, void* pkt, uint64_t precinct, uint64_t zone, uint64_t logical_pe ) {
 
   // Check for valid inputs
-  if (mb_id >= NMBOX) {
-		#if DEBUG
-    char msg[55] = "\ndebug: forza_send_done: INVALID_MBOX\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+  if( mb_id >= NMBOX ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send: INVALID_MBOX\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_MBOX;
-	}
-	if (precinct >= forza_get_num_precincts()) {
-    #if DEBUG
-    char msg[55] = "\ndebug: forza_send_done: INVALID_PRECINCT\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+  }
+  if( pkt == NULL ) {
+#if DEBUG
+    char msg[60] = "\ndebug: forza_send: INVALID_PTR, pkt == NULL\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_PTR;
+  }
+  if( precinct >= forza_get_num_precincts() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send: INVALID_PRECINCT\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_PRECINCT;
   }
-  if (zone >= forza_get_zones_per_precinct()) {
-    #if DEBUG
-    char msg[55] = "\ndebug: forza_send_done: INVALID_ZONE\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+  if( zone >= forza_get_zones_per_precinct() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send: INVALID_ZONE\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_ZONE;
   }
-  if (logical_pe >= forza_get_num_logical_pes()) {
-    #if DEBUG
-    char msg[55] = "\ndebug: forza_send_done: INVALID_LPE\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+  if( logical_pe >= forza_get_num_logical_pes() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send: INVALID_LPE\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_LPE;
   }
 
   // Check zen status
   uint64_t zenstat = forza_read_zen_status();
-  if (zenstat == 0) {
-		char msg[55] = "\ndebug: forza_send_done: zen stat error\n";
-  	rev_write(STDOUT_FILENO, msg, sizeof(msg));
-		assert(0);  // Fix this later to check status bits. This really shouldn't happen now.
-	}
+  if( zenstat == 0 ) {
+    char msg[55] = "\ndebug: forza_send: zenstat error\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    assert( 0 );
+  }
+
+  // Send 7 data words
+  uint64_t* data = pkt;
+  for( int i = 0; i < 7; i++ )
+    forza_send_word( data[i], false );
 
   // Create the control word
-  uint64_t ctrl_word = DONE_OP;
-	ctrl_word = ctrl_word << 40;
-	ctrl_word |= mb_id << 33;
+  uint64_t ctrl_word = mb_id << 33;
   ctrl_word |= precinct << 20;
   ctrl_word != zone << 16;
   //ctrl_word |= ZQM_COMPONENT << 12;
   ctrl_word |= logical_pe;
   forza_send_word( ctrl_word, true );
 
-  uint64_t cntrs = forza_zen_get_cntrs();
+// Debug 0x2222 send: control word and counteri
+#if DEBUG
+  {
+    char msg[50] = "\ndebug: forza_send: 0x2222, ctrl_word, counters\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+  }
+  forza_debug_print( 0x2222, ctrl_word, cntrs );
+#endif
+#if 1
+  uint64_t mb_mask  = 0xFFUL << ( 8 * mb_id );
+  uint64_t mb_count = forza_zen_get_cntrs() & mb_mask;
+  char     msg[55]  = "\ndebug: forza_send: mbx done count: MB=X Count=Y\n";
+  msg[39]           = '0' + mb_id;
+  //msg[41] = '0' + mb_count >> (8 * mb_id);
+  msg[47]           = '0' + forza_get_outstanding_msg_count( mb_id );
+  rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
 
-  // Debug 0x2222 send: control word and counter
-	#if DEBUG
-	{char msg[55] = "\ndebug: forza_send_done: 0x2222, ctrl_word, counters\n";
-  rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-	forza_debug_print( 0x2222, ctrl_word, cntrs);
-	#endif
+  return SUCCESS;
+}
+
+/** @brief Sends a done message to the mailbox at the destination precinct:zone:logocal_pe
+ *   to indiocate that no more messages will be sent from this PE to the destination mailbox
+ *
+ */
+int forza_send_done( uint64_t mb_id, uint64_t precinct, uint64_t zone, uint64_t logical_pe ) {
+
+  // Check for valid inputs
+  if( mb_id >= NMBOX ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send_done: INVALID_MBOX\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_MBOX;
+  }
+  if( precinct >= forza_get_num_precincts() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send_done: INVALID_PRECINCT\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_PRECINCT;
+  }
+  if( zone >= forza_get_zones_per_precinct() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send_done: INVALID_ZONE\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_ZONE;
+  }
+  if( logical_pe >= forza_get_num_logical_pes() ) {
+#if DEBUG
+    char msg[55] = "\ndebug: forza_send_done: INVALID_LPE\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_LPE;
+  }
+
+  // Wait for all messages to that mailbox to be delivered
+  forza_wait_mailbox_done( mb_id );
+  // Check zen status
+  uint64_t zenstat = forza_read_zen_status();
+  if( zenstat == 0 ) {
+    char msg[55] = "\ndebug: forza_send_done: zen stat error\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    assert( 0 );  // Fix this later to check status bits. This really shouldn't happen now.
+  }
+
+  // Create the control word
+  uint64_t ctrl_word = DONE_OP;
+  ctrl_word          = ctrl_word << 40;
+  ctrl_word |= mb_id << 33;
+  ctrl_word |= precinct << 20;
+  ctrl_word != zone << 16;
+  //ctrl_word |= ZQM_COMPONENT << 12;
+  ctrl_word |= logical_pe;
+  forza_send_word( ctrl_word, true );
+
+  // Debug 0x2222 send: control ord and counter
+#if DEBUG
+  {
+    char msg[55] = "\ndebug: forza_send_done: 0x2222, ctrl_word, counters\n";
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+  }
+  forza_debug_print( 0x2222, ctrl_word, cntrs );
+#endif
 
   return SUCCESS;
 }
@@ -237,101 +281,108 @@ int forza_send_done(uint64_t mb_id, uint64_t precinct, uint64_t zone, uint64_t l
  * @brief Checks to see if a message is available at the given mailbox
  *
  */
-int forza_message_available(uint64_t mb_id) {
+int forza_message_available( uint64_t mb_id ) {
 
-	// Check for valid inputs
-  if (mb_id >= NMBOX) {
-		#if DEBUG
+  // Check for valid inputs
+  if( mb_id >= NMBOX ) {
+#if DEBUG
     char msg[55] = "\ndebug: forza_message_available: INVALID_MBOX\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_MBOX;
-	}
+  }
 
-	// Check for message
-	uint64_t zqmstat = forza_read_zqm_status();
-  if ( zqmstat == 0 ) {
+  // Check for message
+  uint64_t zqmstat = forza_read_zqm_status();
+  if( zqmstat == 0 ) {
     return NO_MSG;
-	} else {
-		return SUCCESS;
-	}
+  } else {
+    return SUCCESS;
+  }
 }
 
-/** 
+/**
  * @brief Copies the first message from the specified mailbox into the location
  * pointed to by the pkt pointer. Note that it currently copies 56b and ignores
  * the packet size if it is less than that.
  */
-int forza_message_receive(uint64_t mb_id, uint64_t *pkt, size_t pkt_size) {
+int forza_message_receive( uint64_t mb_id, uint64_t* pkt, size_t pkt_size ) {
 
   // Note: Ignoring pkt_size for now, always reads all 8 words
 
   // Check for valid inputs
-  if (mb_id >= NMBOX) {
-	 	#if DEBUG
+  if( mb_id >= NMBOX ) {
+#if DEBUG
     char msg[55] = "\ndebug: forza_message_receive: INVALID_MBOX\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
     return INVALID_MBOX;
-	}
-  if (pkt == NULL) {
-		#if DEBUG
+  }
+  if( pkt == NULL ) {
+#if DEBUG
     char msg[60] = "\ndebug: forza_message_receive: INVALID_PTR, pkt == NULL\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
-    return INVALID_PTR; 
-	}
-	if (pkt_size > 7 * sizeof(uint64_t)) {
-		#if DEBUG
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_PTR;
+  }
+  if( pkt_size > 7 * sizeof( uint64_t ) ) {
+#if DEBUG
     char msg[55] = "\ndebug: forza_message_receive: INVALID_PKT_SZ\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
-		return INVALID_PKT_SZ;
-	}
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return INVALID_PKT_SZ;
+  }
 
-	// Wait for ZQM ready
+  // Wait for ZQM ready
   uint64_t zqmstat = forza_read_zqm_status();
-	if ( zqmstat == 0 ) {
-		#if DEBUG
+  if( zqmstat == 0 ) {
+#if DEBUG
     char msg[55] = "\ndebug: forza_message_receive: NO_MSG\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));
-    #endif
-		return NO_MSG;
-	}
+    rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+#endif
+    return NO_MSG;
+  }
 
-	// Read control word and check op
-	uint64_t ctrl_word = forza_receive_word ( mb_id );
-  uint64_t msg_op = (ctrl_word & MSG_OP_MASK) >> 40;
-	if (msg_op == DIRECT_OP) {
-		// Read all 7 data words for now (ignore packet size)
-		for( int i = 0; i < 7; i++ )
-    	pkt[i] = forza_receive_word( mb_id ); 
+  // Read control word and check op
+  uint64_t ctrl_word = forza_receive_word( mb_id );
+  uint64_t msg_op    = ( ctrl_word & MSG_OP_MASK ) >> 40;
+  if( msg_op == DIRECT_OP ) {
+    // Read all 7 data words for now (ignore packet size)
+    for( int i = 0; i < 7; i++ )
+      pkt[i] = forza_receive_word( mb_id );
 
-		// Debug 0x3333 receive: msg_op and ZQM status
-		#if DEBUG
-		{char msg[40] = "\ndebug: forza_message_receive: SUCCESS\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-		forza_debug_print ( 0x3333, msg_op, zqmstat );
-		#endif
-		return SUCCESS;
-	} else if (msg_op == INDIRECT_OP) {
-		// Debug 0x3333 receive: msg_op and ZQM status
-		{char msg[70] = "\ndebug: forza_message_receive: INDIRECT_OP not supported\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-    forza_debug_print ( 0x3333, msg_op, zqmstat );
-    assert(0);  // INDIRECT msg op not yet supported
-	} else if (msg_op == DONE_OP) {
-	  #if DEBUG
-		{char msg[40] = "\ndebug: forza_message_receive: DONE_OP\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-		forza_debug_print ( 0x3333, msg_op, zqmstat );
-		#endif
+// Debug 0x3333 receive: msg_op and ZQM status
+#if DEBUG
+    {
+      char msg[40] = "\ndebug: forza_message_receive: SUCCESS\n";
+      rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    }
+    forza_debug_print( 0x3333, msg_op, zqmstat );
+#endif
+    return SUCCESS;
+  } else if( msg_op == INDIRECT_OP ) {
+    // Debug 0x3333 receive: msg_op and ZQM status
+    {
+      char msg[70] = "\ndebug: forza_message_receive: INDIRECT_OP not supported\n";
+      rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    }
+    forza_debug_print( 0x3333, msg_op, zqmstat );
+    assert( 0 );  // INDIRECT msg op not yet supported
+  } else if( msg_op == DONE_OP ) {
+#if DEBUG
+    {
+      char msg[40] = "\ndebug: forza_message_receive: DONE_OP\n";
+      rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    }
+    forza_debug_print( 0x3333, msg_op, zqmstat );
+#endif
     return DONE_MSG;
-	} else {
-		{char msg[50] = "\ndebug: forza_message_receive: Invalid message op\n";
-    rev_write(STDOUT_FILENO, msg, sizeof(msg));}
-		forza_debug_print ( 0x3333, msg_op, zqmstat );
-    assert(0);  // Invalid message op
-	}
+  } else {
+    {
+      char msg[50] = "\ndebug: forza_message_receive: Invalid message op\n";
+      rev_write( STDOUT_FILENO, msg, sizeof( msg ) );
+    }
+    forza_debug_print( 0x3333, msg_op, zqmstat );
+    assert( 0 );  // Invalid message op
+  }
 }
-
