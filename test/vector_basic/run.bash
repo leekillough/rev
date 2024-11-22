853668bea2dec806a9b987778732c892bdbd361c
Kenneth Griesser <kgriesser@tclcomplabs.com>
2024-11-22 07:55:37 -0700

    rest of vector mask instructions

--- test/vector_basic/run.bash
+++ test/vector_basic/run.bash
@@ -15,9 +15,9 @@ echo "Spike passed: see output-spike"
 echo Running Rev
 
 # Production
-# make clean; make -j -s
+make clean; make -j -s
 # Development
-make clean; make TLIST="csr.128.64 csr.64.32 vadd_e16_m1.128.64 vadd_e16_m1.32.16 vadd_e16_m1.64.32 vadd_e16_mf4.128.64 vadd_e32_m1.128.64 vadd_e32_m2.128.64 vadd_e32_m4.128.64 vadd_e32_m8.128.64 vadd_e32_m8_unaligned.128.64 vadd_e32_mf2.128.64 vadd_e64.m1.128.64 vadd_e8_m1.128.64 vadd_e8_mf2.128.64 vadd_e8_mf4.128.64 vadd_e8_mf8.128.64 vec-strcmp.128.64 vec-daxpy.128.64 vec-sgemm.128.64 vec-mixed.128.64"
+# make clean; make TLIST="csr.128.64 csr.64.32 vadd_e16_m1.128.64 vadd_e16_m1.32.16 vadd_e16_m1.64.32 vadd_e16_mf4.128.64 vadd_e32_m1.128.64 vadd_e32_m2.128.64 vadd_e32_m4.128.64 vadd_e32_m8.128.64 vadd_e32_m8_unaligned.128.64 vadd_e32_mf2.128.64 vadd_e64.m1.128.64 vadd_e8_m1.128.64 vadd_e8_mf2.128.64 vadd_e8_mf4.128.64 vadd_e8_mf8.128.64 vec-strcmp.128.64 vec-daxpy.128.64 vec-sgemm.128.64 vec-mixed.128.64"
 
 if [ $? -ne 0 ]; then
     echo "Rev failed: see output/"
