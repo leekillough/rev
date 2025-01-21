b26beb7b04640c9408121c57bb5c017c88fe3e66
John Leidel <john.leidel@gmail.com>
2025-01-21 15:23:03 -0600

    adding a portion of the Forza ISA tests to cover 32, 16 and 8 bit integer amos

---
+++ test/FORZA/forza_isa/amo_r_add8rem_nn.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#define ORIG 0x12
+#define UPD  0x1
+
+#define assert( cond ) \
+  if( !( cond ) ) {    \
+    abort();           \
+  }
+
+int main( int argc, char** argv ) {
+  char          VAL = ORIG;
+  char          MOD = UPD;
+  volatile char RTN = __forza_amo_r_add64rem_nn( &VAL, MOD );
+
+  assert( RTN == ( ORIG + UPD ) );
+  assert( VAL == ( ORIG + UPD ) );
+
+  return 0;
+}
---
+++ test/FORZA/forza_isa/amo_r_or8u.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#define ORIG 0x12
+#define UPD  0x1
+
+#define assert( cond ) \
+  if( !( cond ) ) {    \
+    abort();           \
+  }
+
+int main( int argc, char** argv ) {
+  char VAL = ORIG;
+  char MOD = UPD;
+  __forza_amo_r_or64u( &VAL, MOD );
+
+  assert( VAL == ( ORIG | UPD ) );
+
+  return 0;
+}
